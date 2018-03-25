c58cc451fe6234c9d95acd40bea34af475b46ba7
Brennan Fulmer <brennan.flmr@gmail.com>
2018-03-25 18:24:31 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,12 +1,20 @@
 var playlist = {Slowdive: "Alison", 'My Bloody Valentine': "Sometimes"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
+<<<<<<< HEAD
   playlist[artistName] = songTitle;
+=======
+  playlist.artistName = songTitle;
+>>>>>>> 6263c5aa4a333301a32f33a53566c771e64fb4aa
   return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
+<<<<<<< HEAD
   delete playlist[artistName];
+=======
+  delete playlist.artistName;
+>>>>>>> 6263c5aa4a333301a32f33a53566c771e64fb4aa
   return playlist;
 }
 
